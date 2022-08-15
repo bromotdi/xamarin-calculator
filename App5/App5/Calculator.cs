87e4ad1b130e06f6bf8624cee5d4c4a6d91dfa3b
Daria Bromot <bromotdi@gmail.com>
2022-08-15 18:38:51 +0300

    first commit

---
+++ App5/App5/Calculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App5
+{
+    class Calculator
+    {
+        double a;
+        double b;
+        double c;
+
+        public Calculator(double aa, double bb, double cc)
+        {
+            a = aa;
+            b = bb;
+            c = cc;
+
+        }
+        public double Add()
+        {
+            return a + b;
+        }
+        public double Sub()
+        {
+            return a - b;
+        }
+        public double Mult()
+        {
+            return a * b;
+        }
+        public double Div()
+        {
+            return a / b;
+        }
+        public double Pow()
+        {
+            return Math.Pow(a, b);
+        }
+
+        public double Ex1()
+        {
+            double res = 0;
+            for (double i = a; i < b; i++)
+            {
+
+                if (i % 29 == 0 && i % 2 == 0 && i % 5 == 2)
+                {
+                    res += 1;
+
+                }
+
+            }
+            return res;
+        }
+
+        public double Ex2()
+        {
+            if ((a >= 0) || (b >= 0) || (c >= 0))
+            {
+                return Math.Pow((a + b + c), 2);
+            }
+            else
+            {
+                return a*a + b*b + c*c;
+            }
+        }
+    }
+}
---
+++ App5/App5/MainPage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        Calculator calc;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void input()
+        {
+            a = Convert.ToDouble(x1.Text);
+            b = Convert.ToDouble(x2.Text);
+            c = Convert.ToDouble(x3.Text);
+            calc = new Calculator(a, b, c);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                input();
+                result.Text = calc?.Add().ToString();
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+
+        private void Button_Clicked1(object sender, EventArgs e)
+        {
+            try
+            {
+                input();
+                result.Text = calc?.Sub().ToString();
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+
+        private void Button_Clicked2(object sender, EventArgs e)
+        {
+            try
+            {
+                input();
+                result.Text = calc?.Mult().ToString();
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+
+        private void Button_Clicked3(object sender, EventArgs e)
+        {
+            try
+            {
+                input();
+                result.Text = calc?.Div().ToString();
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+
+        private void Button_Clicked4(object sender, EventArgs e)
+        {
+            try
+            {
+                input();
+                result.Text = calc?.Pow().ToString();
+            }
+            catch
+            {
+                DisplayAlert("Error", "Error string", "Ok");
+            }
+        }
+
+        private void Button_Clicked5(object sender, EventArgs e)
+        {
+            try
+            {
+                input();
+                result.Text = calc?.Ex1().ToString();
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+
+        private void Button_Clicked6(object sender, EventArgs e)
+        {
+            try
+            {
+                input();
+                result.Text = calc?.Ex2().ToString();
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
